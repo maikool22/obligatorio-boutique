a4e18dbd150d9a2fda82b63dd2077bae82d5a3e0
maikool <maikool22@me.com>
2023-06-15 20:14:30 -0300

    dejo solo el bastion

--- instances.tf
+++ instances.tf
@@ -29,35 +29,3 @@ resource "aws_instance" "oblimanual-inst1" {
     ]
   }
 }
-
-resource "aws_instance" "oblimanual-inst2" {
-  ami                    = var.ami-id
-  instance_type          = "t2.micro"
-  key_name               = "vockey"
-  subnet_id              = aws_subnet.oblimanual-subnet2-publica.id
-  vpc_security_group_ids = [aws_security_group.oblimanual-sg.id]
-  tags = {
-    Name      = "oblimanual-inst2"
-    terraform = "True"
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    host        = self.public_ip
-    private_key = file("./vockey.pem")
-  }
-
-  # Dejamos prontos los servicio de git y httpd
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum install -y httpd git",             # -y lo que hace es no pedir confirmacion, instalo httpd y git
-      "sudo systemctl start httpd",                # arranco el servicio httpd
-      "sudo systemctl start httpd",                # dejo habilitado el servicio httpd
-      "sudo touch /var/www/html/index.html",       # creo el index.html vacio
-      "sudo chmod 666 /var/www/html/index.html",   # cambio permisos
-      "sudo echo nodo2 > /var/www/html/index.html" # hago un echo con el nombre del nodo y se lo pongo en el archivo
-    ]
-  }
-}
