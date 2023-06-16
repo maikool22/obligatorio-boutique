3c9ef9dee7844621c10549b44012758fec0acbb5
maikool <maikool22@me.com>
2023-06-16 20:38:04 -0300

    creo variables para vpc

--- resources/variables.tf
+++ resources/variables.tf
@@ -3,3 +3,17 @@ variable "ami-id" {
   type        = string
   default     = "ami-04a0ae173da5807d3" # Id de Amazon Linux
 }
+variable "vpc_cidr" {
+  description = "CIDR para VPC"
+  default     = "10.0.0.0/16"
+}
+/*
+variable "subnet_cidr" {
+  description = "Subnet de CIDR"
+  default     = ""
+}
+*/
+variable "vpc_aws_az" {
+  description = "Zona de disponibilidad para VPC"
+  default     = "us-east-1a"
+}
