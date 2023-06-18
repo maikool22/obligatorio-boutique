4e7872bb4c0cd33dc12d01e5180c9ad85acf96dd
William <william.sandoval@verifone.com>
2023-06-18 17:51:33 -0300

    arreglo el provisioner

--- resources/instances.tf
+++ resources/instances.tf
@@ -17,22 +17,32 @@ resource "aws_instance" "oblimanual-inst1" {
     private_key = file("./vockey.pem")
   }
 
-  # Dejamos prontos los servicio de git y httpd
+
 
   provisioner "remote-exec" {
     inline = [
       "sudo hostnamectl hostname bastion",  #Le configuramos nombre al bastion
       "sudo dnf -y upgrade --releasever=2023.0.20230614", #Actualizamos el core de la ami de linux     
       "sudo yum install -y git", # Instalamos git
+      "mkdir ~/.aws/",
       "sudo mkdir /tmp/obli_deploy/", #creamos carpeta temporal en el bastion
       "sudo git clone https://github.com/maikool22/obligatorio-boutique.git /tmp/obli_deploy/", # clonamos el repo que vamos a usar
       "cd /tmp/obli_deploy/", #nos cambiamos al workdir
       "echo hola!" #aca ejecutamos el deploy de los modulos
 
-      
+
+
       #"sudo touch /var/www/html/index.html",       # creo el index.html vacio
       #"sudo chmod 666 /var/www/html/index.html",   # cambio permisos
       #"sudo echo nodo1 > /var/www/html/index.html" # hago un echo con el nombre del nodo y se lo pongo en el archivo
     ]
+  }
+    provisioner "file" {
+    source      = "~/.aws/credentials"
+    destination = "~/.aws/credentials"
+  }
+    provisioner "file" {
+    source      = "~/.aws/config"
+    destination = "~/.aws/config"
   }
 }
