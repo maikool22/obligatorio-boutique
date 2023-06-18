3b9fcc673023c490fbd233bd44aed3afe5f9345f
William <william.sandoval@verifone.com>
2023-06-18 15:56:49 -0300

    agrego comentarios a las cosas

--- resources/instances.tf
+++ resources/instances.tf
@@ -21,13 +21,15 @@ resource "aws_instance" "oblimanual-inst1" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo hostnamectl hostname bastion",      
-      "sudo dnf -y upgrade --releasever=2023.0.20230614",      
-      "sudo yum install -y git", # -y lo que hace es no pedir confirmacion, instalo httpd y git
-      "sudo mkdir /tmp/obli_deploy/",
-      "sudo git clone https://github.com/maikool22/obligatorio-boutique.git /tmp/obli_deploy/",
-      "cd /tmp/obli_deploy/",
-      "echo hola!"
+      "sudo hostnamectl hostname bastion",  #Le configuramos nombre al bastion
+      "sudo dnf -y upgrade --releasever=2023.0.20230614", #Actualizamos el core de la ami de linux     
+      "sudo yum install -y git", # Instalamos git
+      "sudo mkdir /tmp/obli_deploy/", #creamos carpeta temporal en el bastion
+      "sudo git clone https://github.com/maikool22/obligatorio-boutique.git /tmp/obli_deploy/", # clonamos el repo que vamos a usar
+      "cd /tmp/obli_deploy/", #nos cambiamos al workdir
+      "echo hola!" #aca ejecutamos el deploy de los modulos
+
+      
       #"sudo touch /var/www/html/index.html",       # creo el index.html vacio
       #"sudo chmod 666 /var/www/html/index.html",   # cambio permisos
       #"sudo echo nodo1 > /var/www/html/index.html" # hago un echo con el nombre del nodo y se lo pongo en el archivo
