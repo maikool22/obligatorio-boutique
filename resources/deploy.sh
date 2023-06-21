8f048cc49dbe766e7242c152df275f5439be24ef
William <william.sandoval@verifone.com>
2023-06-21 19:24:50 -0300

    arreglos generales en script

--- resources/deploy.sh
+++ resources/deploy.sh
@@ -2,14 +2,21 @@
 
 #!/bin/bash
 
-# Este script instala con un poco de suerte, Docker y kubectl en Amazon Linux
+# Este script instala con un poco de suerte, Docker y kubectl en Amazon Linux, hace el build, pushea a la registry y despliega en el kluster
 
 
 # 0. Variables:
-# 4. Creo una variable con el uri ID del repo
-ECR_ID=`aws ecr describe-repositories --repository-names cartservice --query 'repositories[].repositoryUri' --output text | cut -d "/" -f1`
 
+# 4. Creo una variable con el uri ID del repo
+# como el comando "aws ecr describre-repositories me trae todo el nombre del repo y solo
+# necesitabamos la url sin el nombre del modulo, optamos por hacer un "cut" para solo obtener el ID"
+
+ECR_ID=$(aws ecr describe-repositories --repository-names cartservice --query 'repositories[].repositoryUri' --output text | cut -d "/" -f1)
+
+# la subcarpeta donde estan los datos para la construccion y despliegue de las imagenes.
 SRC_WORKDIR="/tmp/obli_deploy/src/"
+
+#Aca hacemos una lista con los modulos para despues llamarla con un "for"
 
 MICROSERVICES=(
   "adservice"
@@ -26,19 +33,18 @@ MICROSERVICES=(
   "shippingservice"
 )
 
-
-# 1. Instalar Docker
+# 1. Instalamos Docker
 sudo dnf install telnet docker -y 
 sudo usermod -a -G docker ec2-user
 sudo systemctl enable --now docker
 sudo chmod 666 /var/run/docker.sock
 
-# 2. Instalar kubectl
+# 2. Instalamos kubectl
 sudo curl --silent --location -o /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl --silent --location https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 sudo chmod +x /usr/local/bin/kubectl
 
-# 3. Hago el build de las imagenes
-
+# 3. Con este for, recorro los diferentes directorios src de cada modulo
+# haciendo el Build de imagencorrespondiente
 
 for service in "${MICROSERVICES[@]}"
 do
@@ -49,12 +55,13 @@ do
     cd "$SRC_WORKDIR$service" || continue
   fi
   docker build -t "$service" .
-  cd ../..
 done
 
 
 # 4. Ahora nos logueamos al registry con docker
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_ID
+echo $ECR_ID
+
 
 # 5 hacemos el build de las imagenes
 
@@ -67,9 +74,8 @@ do
     cd "$SRC_WORKDIR$service" || continue
   fi
   docker build -t "$service" .
-  docker tag $service:latest $ECR_ID/$service:latest
-  docker push $ECR_ID/$service:latest
-  cd ../..
+  docker tag "$service:latest" "$ECR_ID/$service:latest"
+  docker push "$ECR_ID/$service:latest"
 done
 
 #ESTO LO VOY A PRECESIAR DESPUES
@@ -81,7 +87,6 @@ do
   echo "Cambio el tag en el Manifest de: $service..." 
   cd "$SRC_WORKDIR/$service/deployment"
   sudo sed -i "s/<IMAGE:TAG>/$ECR_ID\/$service:latest/g" kubernetes-manifests.yaml  
-  cd ../..
 done
 
 
@@ -90,10 +95,11 @@ for service in "${MICROSERVICES[@]}"
 do
   echo "Deployando: $service..." 
   cd "$SRC_WORKDIR/$service/deployment"  
-  kubectl create -f kubernetes-manifests.yaml
-  cd ../..
+  kubectl create -f kubernetes-manifests.yaml 
+
 done
 echo "listo"
 
-
+ENDPOINT=$(kubectl get -o json svc frontend-external | grep hostname)
+echo "el endpoint de la tienda es:  "$ENDPOINT
 
