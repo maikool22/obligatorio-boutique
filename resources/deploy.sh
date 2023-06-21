35de9925a39abde0010355ba1ecb346135554005
William <william.sandoval@verifone.com>
2023-06-21 11:43:23 -0300

    cambios en el deploy.sh

--- resources/deploy.sh
+++ resources/deploy.sh
@@ -4,12 +4,34 @@
 
 # Este script instala con un poco de suerte, Docker y kubectl en Amazon Linux
 
+
+# 0. Variables:
+# 4. Creo una variable con el uri ID del repo
+ECR_ID=`aws ecr describe-repositories --repository-names cartservice --query 'repositories[].repositoryUri' --output text | cut -d "/" -f1`
+
+SRC_WORKDIR= `/tmp/obli_deploy/src/`
+
+MICROSERVICES=(
+  "adservice"
+  "cartservice"
+  "checkoutservice"
+  "currencyservice"
+  "emailservice"
+  "frontend"
+  "loadgenerator"
+  "paymentservice"
+  "productcatalogservice"
+  "recommendationservice"
+  "redis"
+  "shippingservice"
+)
+
+
 # 1. Instalar Docker
 sudo dnf install telnet docker -y 
 sudo usermod -a -G docker ec2-user
 sudo systemctl enable --now docker
 sudo chmod 666 /var/run/docker.sock
-
 
 # 2. Instalar kubectl
 sudo curl --silent --location -o /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl --silent --location https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
@@ -18,31 +40,64 @@ sudo chmod +x /usr/local/bin/kubectl
 # 3. Hago el build de las imagenes
 
 
-#ESTO LO VOY A PRECESIAR DESPUES
-#aws eks update-kubeconfig --region us-east-1 --name oblimanual-kluster
-#kubectl create -f /tmp/obli_deploy/src/adservice/deployment/kubernetes-manifests.yaml
+for service in "${MICROSERVICES[@]}"
+do
+  echo "Haciendo el build para: $service..."
+  if [[ "$service" == "cartservice" ]]; then
+    cd "$SRC_WORKDIR/$service/src" || continue
+  else
+    cd "$SRC_WORKDIR/$service" || continue
+  fi
+  docker build -t "$service" .
+  cd ../..
+done
 
-#cartservice
 
-#esto esta resuelto en terraform
-#aws ecr create-repository --repository-name cartservice
-
-#creo una variable con el uri ID del repo
-ECR_ID=`aws ecr describe-repositories --repository-names cartservice --query 'repositories[].repositoryUri' --output text | cut -d "/" -f1`
-
+# 4. Ahora nos logueamos al registry con docker
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_ID
 
+# 5 hacemos el build de las imagenes
 
-#adservice
-# aws ecr create-repository --repository-name adservice
-cd /tmp/obli_deploy/src/adservice
-docker build -t adservice .
-docker tag adservice:latest $ECR_ID/adservice:latest
-docker push $ECR_ID/adservice:latest
+for service in "${MICROSERVICES[@]}"
+do
+  echo "Haciendo el build para: $service..."
+  if [[ "$service" == "cartservice" ]]; then
+    cd "$SRC_WORKDIR/$service/src" || continue
+  else
+    cd "$SRC_WORKDIR/$service" || continue
+  fi
+  docker build -t "$service" .
+  docker tag $service:latest $ECR_ID/$service:latest
+  docker push $ECR_ID/$service:latest
+  cd ../..
+done
 
-#cartservice
-cd /tmp/obli_deploy/src/cartservice/src
-docker build -t cartservice .
-docker tag cartservice:latest $ECR_ID/cartservice:latest
-docker push $ECR_ID/cartservice:latest
+#ESTO LO VOY A PRECESIAR DESPUES
+aws eks update-kubeconfig --region us-east-1 --name oblimanual-kluster
+
+#Aca le cambio el tag a los manifest de kubernetes
+for service in "${MICROSERVICES[@]}"
+do
+  echo "Cambio el tag en el Manifest de: $service..." 
+  cd "$SRC_WORKDIR/$service/deployment"
+  sed -i "s/<IMAGE:TAG>/$service:latest/g" kubernetes-manifests.yaml
+  
+  cd ../..
+done
+
+
+#Y aca si anda todo, levanto los deploys....
+
+
+#Aca le cambio el tag a los manifest de kubernetes
+for service in "${MICROSERVICES[@]}"
+do
+  echo "Deployando: $service..." 
+  cd "$SRC_WORKDIR/$service/deployment"
+  sed -i "s/<IMAGE:TAG>/$service:latest/g" kubernetes-manifests.yaml
+  kubectl create -f kubernetes-manifests.yaml
+  cd ../..
+done
+
+
 
