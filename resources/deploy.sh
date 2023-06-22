e03e1a8aa3bc9a149e0708ffd1e28de4393d1377
William <william.sandoval@verifone.com>
2023-06-21 21:42:13 -0300

    a ver

--- resources/deploy.sh
+++ resources/deploy.sh
@@ -87,7 +87,8 @@ do
   echo "Cambio el tag en el Manifest de: $service..." 
   cd "$SRC_WORKDIR/$service/deployment"
   aux=$(aws ecr describe-repositories --repository-names $service --query 'repositories[].repositoryUri' --output text)
-  sudo sed -i "s/<IMAGE:TAG>/$aux:latest/g" kubernetes-manifests.yaml  
+  echo $aux
+  sed -i "s/<IMAGE:TAG>/$aux:latest/g" kubernetes-manifests.yaml  
 done
 
 
